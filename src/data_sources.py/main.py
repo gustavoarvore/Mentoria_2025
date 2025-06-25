1aa13e76796a258272ec058223945cfa18394075
Gustavo Machado <gustavo.machado1p@gmail.com>
2025-06-24 22:17:00 -0300

    Resolvendo conflitos após pull

--- src/data_sources.py/main.py
+++ src/data_sources.py/main.py
@@ -1,9 +1,6 @@
 from gupy import buscar_vagas
 
-<<<<<<< HEAD
-=======
 #Função definida para importar outra função de um arquivo já existente
->>>>>>> 02bb25f5f335b675b308ba9ccdecd0d547514181
 def main():
     buscar_vagas()
 
